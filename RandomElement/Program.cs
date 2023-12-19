8a634de99bd185e394a05a14d8c596beca8686f3
TLuht <thomas.luht@tktk.ee>
2023-12-19 23:35:42 +0200

    arrays and such

---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "tortilla", "wrap" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we eat {snacks[randomIndex]}");
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,11 @@
+﻿string hello = "Hello, world!".Trim();
+int spaceCounter = 0;
+
+for (int i = 0; i < hello.Length; i++)
+{
+    if (char.IsWhiteSpace(hello[i]))
+    {
+        spaceCounter++;
+    }
+    Console.WriteLine(spaceCounter + 1);
+}
